621b010e8ccb751c9ecf0c17e225a69629c8fdad
Alex <40795980+AlexProgrammerDE@users.noreply.github.com>
2021-03-13 10:38:36 +0100

    More skinification!

--- modules/luckyperms.js
+++ modules/luckyperms.js
@@ -8,11 +8,11 @@ module.exports = (client) => {
     if (msg.channel.type !== 'text') return
 
     // If the stripped message contains luckyperms
-    if (msg.content.toLowerCase().replace(/\W/gm, '').indexOf('luckyperms') !== -1) {
+    if (msg.content.toLowerCase().replace(/\W/gm, '').indexOf('skinrestorer') !== -1) {
       await msg.channel.send(new discord.MessageEmbed()
-        .setTitle('It looks like you\'re trying to spell LuckPerms!')
-        .setDescription('A useful tip to remember how to spell it is: LUCK is not LUCKY')
-        .setThumbnail('http://lssinfotech.files.wordpress.com/2011/05/clippy.jpg')
+        .setTitle('It looks like you\'re trying to spell SkinsRestorer!')
+        .setDescription('A useful tip to remember how to spell it is: SKINS is not SKIN')
+        .setThumbnail('https://www.spigotmc.org/data/resource_icons/2/2124.jpg')
       )
     }
   })
