586fdb78cdf51e24edf7429ba5219b9f710e6c66
AlexProgrammerDE <40795980+AlexProgrammerDE@users.noreply.github.com>
2025-04-16 12:17:54 +0200

    style: use function

--- src/modules/commands/metadata.ts
+++ src/modules/commands/metadata.ts
@@ -3,13 +3,13 @@ import type { paths } from '@octokit/openapi-types';
 type LatestReleaseResponse =
   paths['/repos/{owner}/{repo}/releases/latest']['get']['responses']['200']['content']['application/json'];
 
-const fetchData = async (): Promise<LatestReleaseResponse> => {
+async function fetchData(): Promise<LatestReleaseResponse> {
   return (await (
     await fetch(
       'https://api.github.com/repos/SkinsRestorer/SkinsRestorer/releases/latest',
     )
   ).json()) as LatestReleaseResponse;
-};
+}
 
 let metadata: LatestReleaseResponse = await fetchData();
 setInterval(() => {
